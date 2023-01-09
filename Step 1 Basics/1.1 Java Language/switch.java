28376b73f8396c8aee247f432ec78fdedc422dd2
Arijit Sarkar <19bcs1705@cuchd.in>
2023-01-09 16:36:57 +0530

    first commit

---
+++ Step 1 Basics/1.1 Java Language/switch.java
@@ -0,0 +1,13 @@
+/* Link : https://practice.geeksforgeeks.org/problems/java-switch-case-statement3529/1 */
+
+class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        switch(choice){
+            
+            case 1:return Math.PI * arr.get(0)*arr.get(0);
+            
+            case 2:return arr.get(0)*arr.get(1);
+        }
+        return 0.0;
+    }
+}
